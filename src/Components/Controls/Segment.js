1889f72c0eb90b0bc98a7cb41fb914e266c84e2c
Rémi Caillot <remicaillot5@gmail.com>
2019-11-14 18:07:21 +0100

    segment refacto

--- src/Components/Controls/Segment.js
+++ src/Components/Controls/Segment.js
@@ -1,32 +1,24 @@
 import {View} from "../View";
 import "./Segment.scss"
 
-export const Segment = (action = () => {}, ...items) => ({
+export const Segment = (action = () => {}, selectedItemValue, ...items) => ({
     ...View()
         .addClass("segment"),
-    selectedItem: 0,
-    select (itemValue) {
-        this.selectedItem = items.findIndex(({value}) => {
-            return value === itemValue;
-        });
-        return this;
-    },
     padding () {
         throw Error("padding can't be set on Segment view");
     },
     get children () {
         const select = (i) => this.select(i);
         return items.map(({value, label}, i) => ({
-            ...View(),
+            ...View()
+                .onClick(() => {
+                    action(value)
+                }),
             classList: {
                 item: true,
-                selected: i === this.selectedItem
+                selected: value === selectedItemValue
             },
             children: [label],
-            onclick (e) {
-                select(value);
-                action(value);
-            }
         }))
     }
 });
