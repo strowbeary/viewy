2b594798e630eebb2546bba7f3f3891cae979e26
Rémi Caillot <remicaillot5@gmail.com>
2020-08-11 11:10:02 +0200

    button icon

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,12 +1,14 @@
 import {View} from "../View/View";
 import {Text, TEXT_STYLE} from "../Text/Text";
 import "./Button.scss";
+import {EmptyView, Icon} from "../..";
 
-export const Button = (
+export const Button = ({
+    icon = null,
     label = "Button",
     action = () => {},
     type = "outlined",
-) => ({
+}) => ({
     ...View()
         .tagName("button")
         .on("click", e => {
@@ -16,6 +18,7 @@ export const Button = (
         .addClass(type),
     get children() {
         return [
+            icon ? Icon(icon) : EmptyView(),
             Text(label, TEXT_STYLE.button),
         ];
     }
