db287faada910256a906cfb57eb6e122e8255b02
Rémi Caillot <r.caillot@maformationofficinale.com>
2020-12-21 10:14:38 +0100

    Better titlebar

--- src/components/TitleBar/TitleBar.js
+++ src/components/TitleBar/TitleBar.js
@@ -3,22 +3,36 @@ import {Text, TEXT_STYLE} from "../Text/Text";
 import {View} from "../View/View";
 import "./TitleBar.scss"
 import {EmptyView} from "../EmptyView";
+import {sp} from "../../ressources/SizingScale";
 
-export const TitleBar = ({title, left_item = EmptyView(), right_item = EmptyView(), bottom_item = EmptyView()}) => ({
+export const TitleBar = ({
+                             title,
+                             left_item = EmptyView(),
+                             right_item = EmptyView(),
+                             bottom_item = EmptyView()
+                         }) => ({
     ...View()
-        .addClass("navigation_bar"),
-    get children() {
+        .addClass("navigation-bar"),
+    get children () {
+        let areas =  left_item.isEmptyView
+            ? `"title title right_item"`
+            : `"left_item . right_item" "title title title"`;
+        if(!bottom_item.isEmptyView) {
+            areas += ` "bottom_item bottom_item bottom_item"`
+        }
+
         return [
             Grid({
                 left_item,
                 right_item,
-                title: title ? Text(title, TEXT_STYLE.largeTitle).addClass("titlebar_title") : EmptyView(),
+                title: title ? Text(title, TEXT_STYLE.largeTitle).addClass("titlebar__title") : EmptyView(),
                 bottom_item,
             })
-                .gap(0, 24)
-                .areas(`"left_item . right_item" "title title title" "bottom_item bottom_item bottom_item"`)
+                .gap(sp(8), sp(14))
+                .areas(areas)
+                .alignItems("center")
                 .columns("auto 1fr auto")
-                .rows("auto auto auto")
+                .autoRows("auto")
 
         ]
     }
